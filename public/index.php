077eb9efdbbead5b9c634761963f01bf8326917f
Bds <essid.badis.pro@gmail.com>
2020-04-28 11:42:22 +0200

    ajout de la boucle

--- public/index.php
+++ public/index.php
@@ -1,3 +1,55 @@
+<?php 
+
+require("../vendor/autoload.php");
+
+use Entity\itemCollection;
+use Entity\user;
+
+$user1 = new User();
+$user1->id = 1;
+$user1->nickname = "GrootBoy";
+$user1->password = "xxxxxxxx";
+
+$user2 = new User();
+$user2->id = 2;
+$user2->nickname = "Léa-iGirl";
+$user2->password = "xxxxxxxx";
+
+
+
+$post1 = new itemCollection();
+$post1->id = 1;
+$post1->namePop = "Negan";
+$post1->reference = "573";
+$post1->categorie = "TELEVISION";
+$post1->picture = "https://cdn.shopify.com/s/files/1/1559/0403/products/negan_1024x1024.jpeg?v=1571948797";
+$post1->user = $user1;
+
+$post2 = new itemCollection();
+$post2->id = 2;
+$post2->namePop = "Bébé Pégase";
+$post2->reference = "383";
+$post2->categorie = "DISNEY";
+$post2->picture = "https://s1.thcdn.com/productimg/1600/1600/11692628-9834556376761334.png";
+$post2->user = $user2;
+
+$post3 = new itemCollection();
+$post3->id = 3;
+$post3->namePop = "Maléfique";
+$post3->reference = "232";
+$post3->categorie = "DISNEY";
+$post3->picture = "https://images-na.ssl-images-amazon.com/images/I/71WOGQDbASL._AC_SL1500_.jpg";
+$post3->user = $user1;
+
+
+$posts = array($post1, $post2, $post3);
+
+
+
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -15,6 +67,8 @@
 
   <!-- Custom styles for this template -->
   <link href="css/blog-home.css" rel="stylesheet">
+
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
 </head>
 
@@ -67,76 +121,137 @@
 
 
 
-
- <!-- Card Wider -->
-<div class="card card-cascade wider">
-
-  <!-- Card image -->
-  <div class="view view-cascade overlay">
-    <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Others/photo6.jpg" alt="Card image cap">
-    <a href="#!">
-      <div class="mask rgba-white-slight"></div>
-    </a>
-  </div>
-
-  <!-- Card content -->
-  <div class="card-body card-body-cascade text-center">
-
-    <!-- Title -->
-    <h4 class="card-title"><strong>Alison Belmont</strong></h4>
-    <!-- Subtitle -->
-    <h5 class="blue-text pb-2"><strong>Graffiti Artist</strong></h5>
-    <!-- Text -->
-    <p class="card-text">Sed ut perspiciatis unde omnis iste natus sit voluptatem accusantium doloremque
-      laudantium, totam rem aperiam. </p>
-
-    <!-- Linkedin -->
-    <a class="px-2 fa-lg li-ic"><i class="fab fa-linkedin-in"></i></a>
-    <!-- Twitter -->
-    <a class="px-2 fa-lg tw-ic"><i class="fab fa-twitter"></i></a>
-    <!-- Dribbble -->
-    <a class="px-2 fa-lg fb-ic"><i class="fab fa-facebook-f"></i></a>
-
-  </div>
-
-</div>
-<!-- Card Wider -->
+<!-- 
+<!-- details card section starts from here -->
+<section class="details-card">
+    <div class="container">
+        <div class="row">
 
 
+<!-- <div class="col-md-4">
+                <div class="card-content">
+                    <div class="card-img">
+                        <img src="https://cdn.shopify.com/s/files/1/1559/0403/products/negan_1024x1024.jpeg?v=1571948797" alt="">
+                        <span><h4>Personal Collection</h4></span>
+                    </div>
+                    <div class="card-desc">
+                        <h3>Negan (573)</h3>
+                        <p>GrootBoy added Negan (573) to this collection </p>
+                            <a href="#" class="btn-card">Partager</a> 
+                            <div class="card-footer text-muted">
+                    Posted on April 23, 2020 by
+                    <a href="#">GrootBoy</a>
+                  </div>  
+                    </div>
+                </div>
+            </div>
 
+            <div class="col-md-4">
+                <div class="card-content">
+                    <div class="card-img">
+                        <img src="img/Exclusive_Funko_Pop_Vinyl-012818.jpg" alt="">
+                        <span><h4>Post</h4></span>
+                    </div>
+                    <div class="card-desc">
+                        <h3>POPS EXCLUSIVES!</h3>
+                        <p>Découvre les dernières figurines Pop! exclusives et édition limitées! On en a pour toutes les collections!</p>
+                            <a href="#" class="btn-card">Partager</a> 
+                            <div class="card-footer text-muted">
+                    Posted on April 22, 2020 by
+                    <a href="#">TheCollectioner</a>
+                  </div>  
+                    </div>
+                </div>
+            </div>
 
-
-
-                <!-- Blog Post -->
-                <div class="card mb-4">
-                  <img class="card-img-top" src="img/Exclusive_Funko_Pop_Vinyl-012818.jpg" alt="Card image cap">
-                  <div class="card-body">
-                    <h2 class="card-title">POPS EXCLUSIVES!</h2>
-                    <p class="card-text">Pour une plus grosse collection ou d'avantage de mise en scène : https://www.popinabox.fr/ Découvre les dernières figurines Pop! exclusives et édition limitées! On en a pour toutes les collections! </p>
-                    <a href="#" class="btn btn-primary">Partage &rarr;</a>
-                  </div>
-                  <div class="card-footer text-muted">
-                    Posted on January 1, 2017 by
+            <div class="col-md-4">
+                <div class="card-content">
+                    <div class="card-img">
+                        <img src="https://images-na.ssl-images-amazon.com/images/I/71WOGQDbASL._AC_SL1500_.jpg" alt="">
+                        <span><h4>Personal Collection</h4></span>
+                    </div>
+                    <div class="card-desc">
+                        <h3>Maléfique (232)</h3>
+                        <p>Alien OneOne added Negan (573) to this collection</p>
+                            <a href="#" class="btn-card">Partager</a>   
+                            <div class="card-footer text-muted">
+                            Posted on April 22, 2020 by
                     <a href="#">Alien OneOne</a>
                   </div>
+                    </div>
                 </div>
-                
+            </div>
 
+            <div class="col-md-4">
+                <div class="card-content">
+                    <div class="card-img">
+                        <img src="https://images-na.ssl-images-amazon.com/images/I/71WOGQDbASL._AC_SL1500_.jpg" alt="">
+                        <span><h4>Personal Collection</h4></span>
+                    </div>
+                    <div class="card-desc">
+                        <h3>Maléfique (232)</h3>
+                        <p>Alien OneOne added Negan (573) to this collection</p>
+                            <a href="#" class="btn-card">Partager</a>   
+                            <div class="card-footer text-muted">
+                            Posted on April 22, 2020 by
+                    <a href="#">Alien OneOne</a>
+                  </div>
+                    </div>
+                </div>
+            </div>
 
+            <div class="col-md-4">
+                <div class="card-content">
+                    <div class="card-img">
+                        <img src="https://images-na.ssl-images-amazon.com/images/I/71WOGQDbASL._AC_SL1500_.jpg" alt="">
+                        <span><h4>Personal Collection</h4></span>
+                    </div>
+                    <div class="card-desc">
+                        <h3>Maléfique (232)</h3>
+                        <p>Alien OneOne added Negan (573) to this collection</p>
+                            <a href="#" class="btn-card">Partager</a>   
+                            <div class="card-footer text-muted">
+                            Posted on April 22, 2020 by
+                    <a href="#">Alien OneOne</a>
+                  </div>
+                    </div>
+                </div>
+            </div> --> 
+            
 
-        <!-- Blog Post -->
-        <div class="card mb-4">
-          <img class="card-img-top" src="http://placehold.it/750x300" alt="Card image cap">
-          <div class="card-body">
-            <h2 class="card-title">Post Title</h2>
-            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!</p>
-            <a href="#" class="btn btn-primary">Read More &rarr;</a>
-          </div>
-          <div class="card-footer text-muted">
-            Posted on January 1, 2017 by
-            <a href="#">Start Bootstrap</a>
-          </div>
+            <div class="col-md-4">
+            <?php 
+            foreach($posts as $post){
+
+            
+            ?>
+                <div class="card-content">
+                    <div class="card-img">
+                        <img src="<?php   echo $post->picture; ?>" alt="">
+                        <span><h4>Personal Collection</h4></span>
+                    </div>
+                    <div class="card-desc">
+                        <h3> <?php echo $post->namePop; ?> ( <?php echo $post->reference; ?> ) </h3>
+                        <p> <?php echo $post->user->nickname;?> added <?php echo $post->namePop; ?> (<?php echo $post->reference; ?>) to this collection</p>
+                            <a href="#" class="btn-card">Partager</a>   
+                            <div class="card-footer text-muted">
+                            Posted on April 22, 2020 by
+                    <a href="#"><?php   echo $post->user->nickname; ?></a>
+                  </div>
+                    </div>
+                </div>
+            </div>
+            <?php } ?>
         </div>
+    </div>
+</section>
+
+
+<!-- details card section starts from here -->
+
+
+ 
+
 
         <!-- Pagination -->
         <ul class="pagination justify-content-center mb-4">
@@ -180,12 +295,23 @@
                     <a href="#">Marvel</a>
                   </li>
                   <li>
-                    <a href="#">DC Comics</a>
+                    <a href="#">DC</a>
                   </li>
                   <li>
                     <a href="#">Harry Potter</a>
                   </li>
-                  
+                  <li>
+                    <a href="#">Fortnite </a>
+                  </li>
+                  <li>
+                    <a href="#">Stranger Things</a>
+                  </li>
+                  <li>
+                    <a href="#">Friends</a>
+                  </li>
+                  <li>
+                    <a href="#">WWE</a>
+                  </li>
                 </ul>
               </div>
               <div class="col-lg-6">
@@ -201,6 +327,18 @@
                   </li>
                   <li>
                     <a href="#">Lord of the Ring</a>
+                  </li>
+                  <li>
+                    <a href="#">My Hero Academia</a>
+                  </li>
+                  <li>
+                    <a href="#">Doctor Who</a>
+                  </li>
+                  <li>
+                    <a href="#">Naruto</a>
+                  </li>
+                  <li>
+                    <a href="#">Pop! Football</a>
                   </li>
                 </ul>
               </div>
@@ -233,8 +371,12 @@
   </footer>
 
   <!-- Bootstrap core JavaScript -->
-  <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+
+  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
 
 </body>
 
---
+++ src/Entity/itemCollection.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Entity;
+
+use Entity\User;
+
+class itemCollection{
+
+public $id;
+public $namePop;
+public $reference;
+public $categorie;
+public $picture;
+public User $user;
+
+}
